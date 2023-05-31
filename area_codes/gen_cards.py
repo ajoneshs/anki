a579ff1a2c2359b72a135c5600229dabbf4d5092
ajoneshs <ajoneshs@gmail.com>
2023-05-31 01:42:36 -0400

    Add ability to extract data from source file

---
+++ area_codes/gen_cards.py
@@ -0,0 +1,23 @@
+import requests
+from bs4 import BeautifulSoup
+
+area_codes_url = "https://www.bennetyee.org/ucsd-pages/area.html"
+html_text = requests.get(area_codes_url).text
+soup = BeautifulSoup(html_text, 'html.parser')
+
+table = soup.find('table')
+
+area_codes = []
+regions = []
+descriptions = []
+
+for row in table.find_all('tr')[1:]:
+    cells = row.find_all('td')
+    
+    area_code = cells[0].text.strip()
+    region = cells[1].text.strip()
+    description = cells[3].text.strip()
+
+    area_codes.append(area_code)
+    regions.append(region)
+    descriptions.append(description)
