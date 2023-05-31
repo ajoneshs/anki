5c95621b029dbaad9d7b380ab474a31ec5b6cb50
ajoneshs <ajoneshs@gmail.com>
2023-05-31 02:49:27 -0400

    Add ability to output data as csv file

--- area_codes/gen_cards.py
+++ area_codes/gen_cards.py
@@ -1,7 +1,9 @@
 import requests
 from bs4 import BeautifulSoup
+import genanki
+import csv
 
-area_codes_url = "https://www.bennetyee.org/ucsd-pages/area.html"
+area_codes_url = 'https://www.bennetyee.org/ucsd-pages/area.html'
 html_text = requests.get(area_codes_url).text
 soup = BeautifulSoup(html_text, 'html.parser')
 
@@ -11,6 +13,7 @@ area_codes = []
 regions = []
 descriptions = []
 
+# extract data from html
 for row in table.find_all('tr')[1:]:
     cells = row.find_all('td')
     
@@ -21,3 +24,13 @@ for row in table.find_all('tr')[1:]:
     area_codes.append(area_code)
     regions.append(region)
     descriptions.append(description)
+
+# generate csv file to import into Anki
+with open('area_code_cards.csv', 'w') as f:
+    writer = csv.writer(f)
+
+    field = ["front", "back", "tag"]
+    writer.writerow(field)
+    
+    for i in range(len(area_codes)):
+        writer.writerow([area_codes[i], f"Region: {regions[i]}\n{descriptions[i]}", regions[i]])
