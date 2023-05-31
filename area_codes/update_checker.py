e312d04b48bb54fffabd0a3b03280546b59e80f9
ajoneshs <ajoneshs@gmail.com>
2023-05-31 02:05:36 -0400

    Minor style change

--- area_codes/update_checker.py
+++ area_codes/update_checker.py
@@ -2,10 +2,10 @@
 
 import requests
 
-og = open("data/area_codes.html", "r")
+og = open('data/area_codes.html', 'r')
 og_file = og.read()
 
-area_codes_url = "https://www.bennetyee.org/ucsd-pages/area.html"
+area_codes_url = 'https://www.bennetyee.org/ucsd-pages/area.html'
 cur_file = requests.get(area_codes_url).text
 
 same_file = og_file == cur_file
@@ -14,7 +14,7 @@ if same_file:
     print("No changes detected")
 else:
     print("Changes detected!")
-    new = open("data/updated_area_codes.html", "w")
+    new = open('data/updated_area_codes.html', 'w')
     new.write(cur_file)
     new.close()
 
