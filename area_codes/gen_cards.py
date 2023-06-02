5b8f1d5190c3b90c620fef46af26abb2d0c718b5
ajoneshs <ajoneshs@gmail.com>
2023-06-02 05:27:01 -0400

    Add ability to generate cards with image links

--- area_codes/gen_cards.py
+++ area_codes/gen_cards.py
@@ -24,12 +24,26 @@ for row in table.find_all('tr')[1:]:
     regions.append(region)
     descriptions.append(description)
 
+# load lists of available images
+with open('data/pics/tz.csv', 'r') as f:
+    tz_csv = f.read()
+with open('data/pics/usa.csv', 'r') as f:
+    usa_csv = f.read()
+
 # generate csv file to import into Anki
-with open('data/area_code_cards.csv', 'w') as f:
+with open('data/area_code_cards.csv', 'w', newline='') as f:
     writer = csv.writer(f)
 
     field = ["front", "back", "tag"]
-    writer.writerow(field)
+    #writer.writerow(field)
     
     for i in range(len(area_codes)):
-        writer.writerow([area_codes[i], f"Region: {regions[i]}\n{descriptions[i]}", regions[i]])
+        # generating maps field
+        tz_img_exists = area_codes[i] in tz_csv
+        usa_img_exists = area_codes[i] in usa_csv
+        # maybe need to str(area_codes[i])
+        maps_field = (
+            ('<img src="tz' + area_codes[i] + '.png">') * tz_img_exists + '<br>' + 
+            ('<img src="usa' + area_codes[i] + '.png">') * usa_img_exists
+        )
+        writer.writerow([area_codes[i], f"Region: {regions[i]}<br>{descriptions[i]}", maps_field, "AC::" + regions[i]])
