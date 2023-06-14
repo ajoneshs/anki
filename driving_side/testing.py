e9ac6fae1817ec2e944be193438f8d8a38027687
ajoneshs <ajoneshs@gmail.com>
2023-06-13 20:04:15 -0400

    Diagnose but not solve issue with countries

--- driving_side/testing.py
+++ driving_side/testing.py
@@ -8,17 +8,37 @@ html_text = requests.get(driving_side_url).text
 soup = BeautifulSoup(html_text, 'html.parser')
 
 tables = soup.find_all('table')
-#print(tables)
-print(len(tables))
-print(tables[1].prettify()[0:2000])
+#print(len(tables))
+#print(tables[1].prettify()[0:2000])
+# Table with countries and LHT or RHT is tables[1]
+table = tables[1]
 
+countries = []
+sides = []
+
+
+################
+# doesn't work for Canada, China, UK, US because of the sub-categories
+for row in table.find_all('tr')[1:]:
+    cells = row.find_all('td')
+
+    country = cells[0].text.strip()
+    side = cells[1].text.strip()
+    abc = ['RHT', 'LHT']
+    if side[0:3] not in abc:
+        continue
+
+    countries.append(country)
+    sides.append(side)
+
+print(sides)
+print(len(sides))
 
 '''
-#print(soup.prettify())
 
-start_string = "Worldwide distribution by country"
+with open('driving_side_cards.csv', 'w', newline='') as f:
+    writer = csv.writer(f)
 
-print(type(soup))
+# add tags
 
-re.findall(start_string, soup)
 '''
