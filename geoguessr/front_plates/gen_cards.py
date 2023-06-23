83165edd0ec62e387a75a9acb81ad5fd690f1085
ajoneshs <ajoneshs@gmail.com>
2023-06-23 00:58:10 -0400

    Working card generation

--- geoguessr/front_plates/gen_cards.py
+++ geoguessr/front_plates/gen_cards.py
@@ -3,27 +3,29 @@ from data.state_abbrev import abbrev
 
 data = open('data/data.txt', 'r').read().splitlines()
 
-front = data[1].split(', ')
-rear = data[3].split(', ')
-most = data[5].split(', ')
+both = data[1].split(', ')
+rear_only = data[3].split(', ')
+most_both = data[5].split(', ')
+
+master = []
+
 
 # tag format: Geo::Country::US::State/territory Geo::Front_Plates
+def add_to_master(state_list, req):
+    for state in state_list:
+        tags = f"Geo::Country::US::{abbrev[state]} Geo::Front_Plates::{req.replace(' ', '_')}"
+        master.append([state, req, tags])
 
-'''
+
+add_to_master(both, 'front and rear')
+add_to_master(rear_only, 'only rear')
+add_to_master(most_both, 'typically front and rear')
+
 with open('data/front_plates_cards.csv', 'w', newline='') as f:
     writer = csv.writer(f)
 
-    field = ["front", "back", "tag"]
+    field = ["location", "requirements", "tag"]
     #writer.writerow(field)
     
-    for i in range(len(area_codes)):
-        # generating maps field
-        tz_img_exists = area_codes[i] in tz_csv
-        usa_img_exists = area_codes[i] in usa_csv
-        # maybe need to str(area_codes[i])
-        maps_field = (
-            ('<img src="tz' + area_codes[i] + '.png">') * tz_img_exists + '<br>' + 
-            ('<img src="usa' + area_codes[i] + '.png">') * usa_img_exists
-        )
-        writer.writerow([area_codes[i], f"Region: {regions[i]}<br>{descriptions[i]}", maps_field, "AC::" + regions[i]])
-'''
+    for i in master:
+        writer.writerow(i)
