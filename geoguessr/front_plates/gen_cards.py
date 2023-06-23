c9e6ed950cd3f6233b760cab0e6cdc6399db44b6
ajoneshs <ajoneshs@gmail.com>
2023-06-22 23:58:48 -0400

    Initial data processing

--- geoguessr/front_plates/gen_cards.py
+++ geoguessr/front_plates/gen_cards.py
@@ -1,1 +1,29 @@
 import csv
+
+data = open('data/data.txt', 'r').read().splitlines()
+
+front = data[1]
+rear = data[3]
+most = data[5]
+
+
+'''
+
+with open('data/front_plates_cards.csv', 'w', newline='') as f:
+    writer = csv.writer(f)
+
+    field = ["front", "back", "tag"]
+    #writer.writerow(field)
+    
+    for i in range(len(area_codes)):
+        # generating maps field
+        tz_img_exists = area_codes[i] in tz_csv
+        usa_img_exists = area_codes[i] in usa_csv
+        # maybe need to str(area_codes[i])
+        maps_field = (
+            ('<img src="tz' + area_codes[i] + '.png">') * tz_img_exists + '<br>' + 
+            ('<img src="usa' + area_codes[i] + '.png">') * usa_img_exists
+        )
+        writer.writerow([area_codes[i], f"Region: {regions[i]}<br>{descriptions[i]}", maps_field, "AC::" + regions[i]])
+
+'''
