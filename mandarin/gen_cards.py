4550bf60fb9588be5a4e51a7ce741c53ee6d8563
ajoneshs <ajoneshs@gmail.com>
2023-06-26 23:49:37 -0400

    Add code skeleton

--- mandarin/gen_cards.py
+++ mandarin/gen_cards.py
@@ -1,1 +1,26 @@
 import csv
+
+count = 0
+cards = []
+
+
+def create_new_row():
+    row = []
+    row = [1, 2, 3]
+    return row
+
+
+while True:
+    count += 1
+    if count == 1:
+        print("Welcome, would you like to add cards? y/n")
+    else:
+        print("Add more cards? y/n")
+    
+    if input() == "n":
+        break
+    else: 
+        row = create_new_row()
+        cards.append(row)
+
+print(cards)
