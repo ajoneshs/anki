f3bcfe444e50ee145904e002e0cdf2a33f328aeb
ajoneshs <ajoneshs@gmail.com>
2023-08-23 02:39:22 -0400

    Test what happens with non-existent trad version

--- mandarin/si_to_tr.py
+++ mandarin/si_to_tr.py
@@ -1,3 +1,9 @@
 import opencc
 converter = opencc.OpenCC('s2t.json')
-print(converter.convert('汉字'))  # 漢字
+men_si = "们"
+print(converter.convert('们'))  # 漢字
+print(converter.convert('們'))
+
+# example without traditional equivalent
+ni = "你"
+print(converter.convert(ni))
