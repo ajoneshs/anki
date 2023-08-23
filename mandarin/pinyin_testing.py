de037c5fdd82c4906ae536c31b0319eed8ee397a
ajoneshs <ajoneshs@gmail.com>
2023-08-23 02:12:24 -0400

    More testing

--- mandarin/pinyin_testing.py
+++ mandarin/pinyin_testing.py
@@ -3,3 +3,7 @@ import pinyin
 print(pinyin.get('你好'))
 
 print(pinyin.get('你好', format="numerical"))
+
+print(pinyin.get('你好', format="numerical", delimiter=" "))
+
+print(pinyin.get('你好', format="strip", delimiter=" "))
