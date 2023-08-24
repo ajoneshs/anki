6dea254db615166e55b55e5936b5429c8ab514ca
ajoneshs <ajoneshs@gmail.com>
2023-08-23 21:45:09 -0400

    test what happens when no def found

--- mandarin/pinyin_testing.py
+++ mandarin/pinyin_testing.py
@@ -9,3 +9,10 @@ print(pinyin.get('你好', format="numerical", delimiter=" "))
 print(pinyin.get('你好', format="strip", delimiter=" "))
 
 print(pinyin.cedict.translate_word('你'))
+
+print(pinyin.cedict.translate_word('随机胡言乱语'))
+
+(print("-------"))
+non_existent_def = pinyin.cedict.translate_word('随机胡言乱语')
+print(non_existent_def)
+print(type(non_existent_def))
