17d068c76fa012f926e59154e15f327776a60e8d
ajoneshs <ajoneshs@gmail.com>
2023-08-25 02:33:06 -0400

    Add notes about playsound bug

--- mandarin/msu/test.py
+++ mandarin/msu/test.py
@@ -12,13 +12,20 @@ syllable1 = "nǐ"
 syllable2 = "xióng"
 id = (pinyin_ids[syllable2])[4]
 print(id)
+#### Important Note ####
+# For some reason, playsound does not like long file names and it will not
+# work when the filename is too long
+# It does not clearly communicate this in the error message
+# Be careful changing `filename`!!!
+########################
+filename = f"{syllable2}.mp3"
 
 #id = 1662
 url1 = f"https://tone.lib.msu.edu/tone/{id}/PROXY_MP3/download"
 url2 = f"https://tone.lib.msu.edu/tone/{id}/PROXY_MP3/view"
 
 downloaded_audio = requests.get(url1)
-with open(f'audio/{id}.mp3', 'wb') as f:
+with open(f'audio/{filename}', 'wb') as f:
     f.write(downloaded_audio.content)
 
 # shouldn't it be possible to access this file without having to change the directory??
@@ -26,7 +33,10 @@ with open(f'audio/{id}.mp3', 'wb') as f:
 os.chdir('audio')
 print(os.getcwd())
 
-playsound(f'{id}.mp3')
+print(filename)
+print(os.listdir())
+#filename = "1662.mp3"
+playsound(filename)
 
 # filename should be: (msu)_(syllable with tone)_(id #).mp3
 # i.e. msu_nǐ_8953.mp3
