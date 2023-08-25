1b391305d1d00a76347446a638333789eb7e7fec
ajoneshs <ajoneshs@gmail.com>
2023-08-25 01:50:56 -0400

    Combine with other testing file

--- mandarin/msu/test.py
+++ mandarin/msu/test.py
@@ -1,10 +1,18 @@
 from playsound import playsound
 import requests
 import os
+import json
 
 print(os.getcwd())
 
-id = 1662
+f = open('pinyin_ids.json')
+pinyin_ids = json.load(f)
+
+syllable1 = "nǐ"
+syllable2 = "xióng"
+id = (pinyin_ids[syllable2])[4]
+
+#id = 1662
 url1 = f"https://tone.lib.msu.edu/tone/{id}/PROXY_MP3/download"
 url2 = f"https://tone.lib.msu.edu/tone/{id}/PROXY_MP3/view"
 
