cda936d02a0c342a803272e73f4cb89439228482
ajoneshs <ajoneshs@gmail.com>
2023-08-25 01:52:41 -0400

    Add note about possible filename

--- mandarin/msu/test.py
+++ mandarin/msu/test.py
@@ -11,6 +11,7 @@ pinyin_ids = json.load(f)
 syllable1 = "nǐ"
 syllable2 = "xióng"
 id = (pinyin_ids[syllable2])[4]
+print(id)
 
 #id = 1662
 url1 = f"https://tone.lib.msu.edu/tone/{id}/PROXY_MP3/download"
@@ -24,3 +25,6 @@ os.chdir('audio')
 print(os.getcwd())
 
 playsound(f'{id}.mp3')
+
+# filename should be: (msu)_(syllable with tone)_(id #).mp3
+# i.e. msu_nǐ_8953.mp3
