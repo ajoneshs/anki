c75332896ac52ace301674e6af77026f7fb8bc7a
ajoneshs <ajoneshs@gmail.com>
2023-08-30 01:16:43 -0400

    Add basic url status checking

--- mandarin/img_availability.py
+++ mandarin/img_availability.py
@@ -1,9 +1,18 @@
 # source for data: https://github.com/ruddfawcett/hanziDB.csv/blob/master/data/hanziDB.csv
+import requests
+import csv
 
 row_num = 0
-with open('hanziDB.csv', encoding='utf-8') as hanzi:
-    for row in hanzi:
+with open('hanziDB.csv', encoding='utf-8') as hanzifile:
+    hanzireader = csv.reader(hanzifile, delimiter=',')
+    for row in hanzireader:
         row_num += 1
-        if row_num > 300:
+        if row_num > 5:
             break
         print(row)
+        char = row[1]
+        url_template = f"https://commons.wikimedia.org/wiki/File:{char}-bw.png"
+        print(char)
+        print(url_template)
+        r = requests.get(url_template)
+        print(r.status_code)
