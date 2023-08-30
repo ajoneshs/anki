20555ef036a87c7c78b6234d7e8657d016dbaebd
ajoneshs <ajoneshs@gmail.com>
2023-08-30 00:58:15 -0400

    Add parsing csv contents

--- mandarin/img_availability.py
+++ mandarin/img_availability.py
@@ -1,1 +1,9 @@
 # source for data: https://github.com/ruddfawcett/hanziDB.csv/blob/master/data/hanziDB.csv
+
+row_num = 0
+with open('hanziDB.csv', encoding='utf-8') as hanzi:
+    for row in hanzi:
+        row_num += 1
+        if row_num > 300:
+            break
+        print(row)
