9eef79baca88637668f3c4ee5c09f7ab87aac7d8
ajoneshs <ajoneshs@gmail.com>
2023-09-07 01:09:15 -0400

    Remove old comment

--- mandarin/gen_cards.py
+++ mandarin/gen_cards.py
@@ -1,12 +1,3 @@
-# automatically add handwritten image for writing cards
-# maybe use https://github.com/chanind/hanzi-writer
-# https://commons.wikimedia.org/wiki/Commons:Stroke_Order_Project might also be useful
-
-# Wikipedia stroke order
-#url_template = f"https://commons.wikimedia.org/wiki/File:{char}-bw.png"
-# doesn't seem to have traditional characters, i.e. nothing for 們
-
-
 '''
 To-Do
 * figure out source for stroke order
